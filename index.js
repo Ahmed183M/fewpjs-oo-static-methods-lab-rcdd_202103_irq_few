f4c61d529b43d060bcc31bf1816824e9059ae997
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ class Formatter {
   }
   
   static capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+     if(!sentence.split) return sentence;
+    var _titleizeWord = function(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+        },
+        result = [];
+    sentence.split(" ").forEach(function(w) {
+        result.push(_titleizeWord(w));
+    });
+    return result.join(" ");
   }
 }
