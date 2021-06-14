0154a75d8b28b433db7bcf7f60e8c5e4fa857138
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ class Formatter {
   static capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
+  
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 }
