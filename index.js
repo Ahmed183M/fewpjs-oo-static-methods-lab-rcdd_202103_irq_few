60a9692667545c3678b437c6dc3aa2893bc08df7
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Formatter {
   static titleize(sentence) {
      if(!sentence.split) return sentence;
     var _titleizeWord = function(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+            if(string != )return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
         },
         result = [];
     sentence.split(" ").forEach(function(w) {
