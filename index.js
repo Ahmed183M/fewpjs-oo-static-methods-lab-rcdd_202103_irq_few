8a6176d1217657de25dc9dfc33c036c4ba88529e
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ class Formatter {
     string[0]=string[0].toUpperCase();
     return string;
   }
+  
+  static sanitize(string){
+    string[0]=string[0].toUpperCase();
+    return string;
+  }
+  
+  static titleize(string){
+    string[0]=string[0].toUpperCase();
+    return string;
+  }
 }
